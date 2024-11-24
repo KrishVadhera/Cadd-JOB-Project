41e7861b6524ed67e8210361a638bf27db37d8a9
KrishVadhera <krish.vadhera702@gmail.com>
2024-11-24 17:38:03 +0530

    Add files via upload

---
+++ BCD_tb.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 24.11.2024 17:03:51
+// Design Name: 
+// Module Name: BCD_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module BCD_tb(
+
+    );
+endmodule
